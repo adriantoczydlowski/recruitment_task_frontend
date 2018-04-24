2612e6a84ddbff28196270049ddbe5ccc7ef7606
Adrian Toczydłowski <atoczydlowski@ownedoutcomes.com>
2018-04-24 02:36:46 +0200

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
+  tasks$;
+  data;
+
+  @ViewChild('fileInput') fileInput: ElementRef;
+
+  constructor(private fb: FormBuilder, private http: Http) {
+    this.getTasks();
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      avatar: null
+    });
+  }
+
+  onFileChange(event) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        console.log('data', reader.result);
+        this.form.get('avatar').setValue({
+          filename: file.name,
+          filetype: file.type,
+          value: reader.result.split(',')[1]
+        });
+      };
+    }
+  }
+
+  onSubmit() {
+    const formModel = this.form.value;
+    this.loading = true;
+    this.http.post('api/tasks/upload', formModel).subscribe(() => {
+      this.loading = false;
+      this.getTasks();
+      alert('done!');
+    });
+    // setTimeout(() => {
+    //   console.log(formModel);
+    //   alert('done!');
+    //   this.loading = false;
+    // }, 1000);
+  }
+
+  clearFile() {
+    this.form.get('avatar').setValue(null);
+    this.fileInput.nativeElement.value = '';
+  }
+
+  private getTasks() {
+    this.tasks$ = this.http
+      .get('api/tasks')
+      .pipe(map(res => res.json()))
+      .subscribe(data => (this.data = data));
+  }
 }
